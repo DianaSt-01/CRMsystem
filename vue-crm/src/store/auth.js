05fbf9b3795908b34ae16db4df53fe0d653a423f
Diana <starvit1978@mail.ru>
2020-03-24 15:57:36 +0300

    AddCurrency

--- vue-crm/src/store/auth.js
+++ vue-crm/src/store/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 /* eslint-disable comma-spacing */
 import firebase from 'firebase/app'
 
@@ -8,8 +9,30 @@ export default {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password)
       } catch (e) {
+        commit('setError', e)
         throw e
       }
+    },
+    async register ({ dispatch, commit }, { email, password, name }) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        const uid = await dispatch('getUid')
+        await firebase.database().ref(`/users/${uid}/info`).set({
+          bill: 10000,
+          name
+        })
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
+    getUid () {
+      const user = firebase.auth().currentUser
+      return user ? user.uid : null
+    },
+    async logout ({ commit }) {
+      await firebase.auth().signOut()
+      commit('clearInfo')
     }
   }
 }
--- vue-crm/src/store/index.js
+++ vue-crm/src/store/index.js
@@ -1,17 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import info from './info'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    error: null
   },
   mutations: {
+    setError (state, error) {
+      state.error = error
+    },
+    clearError (state) {
+      state.error = null
+    }
   },
   actions: {
+    async fetchCurrency () {
+      const res = await fetch('http://data.fixer.io/api/latest?access_key=d382440850e56eb8024ab3c00bc18dd2&symbols=USD,EUR,RUB')
+      // eslint-disable-next-line no-return-await
+      return await res.json()
+    }
+  },
+  getters: {
+    error: s => s.error
   },
   modules: {
-    auth
+    auth, info
   }
 })
